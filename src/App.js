fcc7533230779e5f1e937d762ee9f4752f09429d
Elias Hantula <ehantula@indiana.edu>
2017-12-18 13:31:39 -0500

    nav-bar

--- src/App.js
+++ src/App.js
@@ -13,8 +13,23 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 
 
-const NavBar = () => (
+const Navbar = () => (
+
   
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Odin's Mead</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="">Welcome</a></li>
+      <li><a href="#">Reservations</a></li>
+      <li><a href="#">Menu</a></li>
+       <li><a href="#">Events</a></li>
+       <li><a href="#">Contact</a></li>
+         <li><a href="#">VIP</a></li>
+    </ul>
+  </div>
 
 );
 
@@ -22,10 +37,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar className="navbar 
+        <Navbar className="navbar navbar-inverse"/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          //<img src={logo} className="App-logo" alt="logo" />
+          //<h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
